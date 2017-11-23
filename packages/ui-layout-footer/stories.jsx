8ecc15923df0d041e3e7191c74ddd84c5198ad14
Marco Biedermann <marco.biedermann@gmx.de>
2017-11-23 23:22:47 +0100

    add storybook a11y addon

--- packages/ui-layout-footer/stories.jsx
+++ packages/ui-layout-footer/stories.jsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs';
+import { checkA11y } from 'storybook-addon-a11y';
 /* eslint-enable */
 
 import './index.css';
 
 storiesOf('Footer/1.0.0', module)
+  .addDecorator(checkA11y)
   .addDecorator(withKnobs)
   .addWithStaticMarkup('default', () => (
     <footer className="footer">
