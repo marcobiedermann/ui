4f4e85b5eccc3651a7f8240bfd41a021f08e6c8f
Marco Biedermann <marco.biedermann@gmx.de>
2017-11-26 23:40:13 +0100

    add ui-module-list

---
+++ packages/ui-modules-list/stories.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { checkA11y } from 'storybook-addon-a11y';
+/* eslint-enable */
+
+import './index.css';
+
+storiesOf('List/1.0.0', module)
+  .addDecorator(checkA11y)
+  .addWithStaticMarkup('definition list', () => (
+    <dl>
+      <dt>Coffee</dt>
+      <dd>Black hot drink</dd>
+      <dt>Milk</dt>
+      <dd>White cold drink</dd>
+    </dl>
+  ))
+  .addWithStaticMarkup('ordered list', () => (
+    <ol>
+      <li>List Item 1</li>
+      <li>List Item 2
+        <ol>
+          <li>Sub List Item 1</li>
+          <li>Sub List Item 2</li>
+          <li>Sub List Item 3</li>
+        </ol>
+      </li>
+      <li>List Item 3</li>
+      <li>List Item 4</li>
+      <li>List Item 5</li>
+    </ol>
+  ))
+  .addWithStaticMarkup('unordered list', () => (
+    <ul>
+      <li>List Item 1</li>
+      <li>List Item 2
+        <ul>
+          <li>Sub List Item 1</li>
+          <li>Sub List Item 2</li>
+          <li>Sub List Item 3</li>
+        </ul>
+      </li>
+      <li>List Item 3</li>
+      <li>List Item 4</li>
+      <li>List Item 5</li>
+    </ul>
+  ));
