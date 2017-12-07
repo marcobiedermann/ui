2490ed99c0e0124985f3ea3ab58f80934a62aab9
Marco Biedermann <marco.biedermann@gmx.de>
2017-12-07 23:52:43 +0100

    add a11y addon to global config

--- packages/ui-modules-canvas/stories.jsx
+++ packages/ui-modules-canvas/stories.jsx
@@ -2,13 +2,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, number } from '@storybook/addon-knobs';
-import { checkA11y } from 'storybook-addon-a11y';
 /* eslint-enable */
 
 import './index.css';
 
 storiesOf('Canvas/1.0.0', module)
-  .addDecorator(checkA11y)
   .addDecorator(withKnobs)
   .addWithStaticMarkup('default', () => (
     <canvas width={number('width', '800')} height={number('height', '600')} />
