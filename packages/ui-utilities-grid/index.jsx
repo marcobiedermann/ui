f94772e4ba229de0226785f5f8f1b3a635feff68
Marco Biedermann <marco.biedermann@gmx.de>
2018-09-06 23:12:12 +0200

    add grid columns

--- packages/ui-utilities-grid/index.jsx
+++ packages/ui-utilities-grid/index.jsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './style.css';
@@ -16,4 +17,42 @@ Grid.defaultProps = {
   children: null,
 };
 
+const GridRow = ({ children }) => (
+  <div className={styles.grid__row}>
+    {children}
+  </div>
+);
+
+GridRow.propTypes = {
+  children: PropTypes.node,
+};
+
+GridRow.defaultProps = {
+  children: null,
+};
+
+const GridColumn = ({ children, span }) => (
+  <div
+    className={classNames(styles.grid__column, {
+      [styles[`grid__column--${span}`]]: span,
+    })}
+  >
+    {children}
+  </div>
+);
+
+GridColumn.propTypes = {
+  children: PropTypes.node,
+  span: PropTypes.string,
+};
+
+GridColumn.defaultProps = {
+  children: null,
+  span: '12',
+};
+
 export default Grid;
+export {
+  GridRow,
+  GridColumn,
+};
--- packages/ui-utilities-grid/index.test.jsx
+++ packages/ui-utilities-grid/index.test.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Grid from '.';
+import Grid, { GridRow, GridColumn } from '.';
 
 describe('Grid component', () => {
+  const GRID_COLUMNS = 12;
+
   it('renders correctly', () => {
     const tree = renderer
       .create(<Grid />)
@@ -11,4 +13,29 @@ describe('Grid component', () => {
 
     expect(tree).toMatchSnapshot();
   });
+
+  for (let i = 1; i < GRID_COLUMNS; i += 1) {
+    it(`renders column span ${i} correctly`, () => {
+      const tree = renderer
+        .create(
+          <Grid>
+            <GridRow>
+              <GridColumn span={`${i}`}>
+                Column
+                {' '}
+                {i}
+              </GridColumn>
+              <GridColumn span={`${GRID_COLUMNS - i}`}>
+                Column
+                {' '}
+                {GRID_COLUMNS - i}
+              </GridColumn>
+            </GridRow>
+          </Grid>,
+        )
+        .toJSON();
+
+      expect(tree).toMatchSnapshot();
+    });
+  }
 });
