361c58938ee583706d6d491ac07608b28d4f590f
Marco Biedermann <marco.biedermann@gmx.de>
2018-09-06 23:26:14 +0200

    add address module

---
+++ packages/ui-modules-address/README.md
@@ -0,0 +1,34 @@
+# @marcobiedermann/ui-modules-anchor
+
+[![npm version](https://badge.fury.io/js/%40marcobiedermann%2Fui-modules-anchor.svg)](https://badge.fury.io/js/%40marcobiedermann%2Fui-modules-anchor)
+
+## Table of contents
+
+* [Requirements](#requirements)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+
+## Requirements
+
+* [Node.js](https://nodejs.org)
+* [postcss](https://github.com/postcss/postcss)
+* [postcss-import](https://github.com/postcss/postcss-import)
+
+## Installation
+
+```sh
+npm install @marcobiedermann/ui-modules-anchor
+```
+
+## Usage
+
+```css
+@import '@marcobiedermann/ui-modules-anchor';
+```
+
+## License
+
+[MIT License](../../LICENSE)
+
+Copyright (c) 2017 Marco Biedermann
---
+++ packages/ui-modules-address/docs/stories.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
+
+import '../style.css';
+
+storiesOf('Address/1.0.0', module)
+  .addDecorator(withKnobs)
+  .addWithStaticMarkup('default', () => (
+    <address itemScope itemType="http://schema.org/Organization">
+      <span itemProp="name">
+        {text('Company', 'Company Name')}
+      </span>
+      <div itemProp="address" itemScope itemType="http://schema.org/PostalAddress">
+        <span itemProp="streetAddress">
+          {text('Street Address', 'Street Address')}
+        </span>
+        <br />
+        <span itemProp="postalCode">
+          {text('Postal Code', 'Postal Code')}
+        </span>
+        {' '}
+        <span itemProp="addressLocality">
+          {text('City', 'City Name')}
+        </span>
+        <br />
+        <span itemProp="addressCountry">
+          {text('Country', 'Country')}
+        </span>
+      </div>
+    </address>
+  ));
