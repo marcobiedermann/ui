c313f660ff1a5e519cd91c51101d8c81309620f6
Marco Biedermann <marco.biedermann@gmx.de>
2018-09-18 22:40:52 +0200

    add figure component

---
+++ packages/ui-modules-figure/docs/stories.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import '../figure.css';
+
+storiesOf('Figure', module)
+  .addWithStaticMarkup('default', () => (
+    <figure />
+  ));
