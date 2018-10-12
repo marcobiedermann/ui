d02a50576818de38e840ec71fa19e2c129034310
Marco Biedermann <marco.biedermann@gmx.de>
2018-10-12 17:00:36 +0200

    feat: dependencies
    Move devDependencies to dependencies

--- packages/ui-modules-media/react/Media.test.jsx
+++ packages/ui-modules-media/react/Media.test.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Media, { MediaObject, MediaBody } from '.';
--- packages/ui-utilities-grid/react/Grid.test.jsx
+++ packages/ui-utilities-grid/react/Grid.test.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Grid, { GridRow, GridColumn } from '.';
